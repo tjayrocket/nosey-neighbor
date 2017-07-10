92a3de53a02d17a3784c74ccfd6fc31a460a586e
Stephanie Dover <doverstephaniem@gmail.com>
2017-07-10 13:44:58 -0700

    fixed comment schema for naming conventions

--- model/comment.js
+++ model/comment.js
@@ -3,9 +3,9 @@
 const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
-  userID: { type: mongoose.Schema.Types.ObjectId, required: true },
-  incidentID: { type: mongoose.Schema.Types.ObjectId, required: true },
-  commentID: { type: mongoose.Schema.Types.ObjectId, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  incidentId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  commentId: { type: mongoose.Schema.Types.ObjectId, required: true },
   content: { type: String, required: true, minlength: 1 },
   timeStamp: { type: Date, required: true }
 });
