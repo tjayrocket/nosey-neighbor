b4ddfeea4ec9ddadc46862158ff88980de9f711c
Spencer Gietzen <Speng1516@comcast.net>
2017-07-11 13:28:35 -0700

    profilerouter get post put

--- router/profile-router.js
+++ router/profile-router.js
@@ -4,26 +4,32 @@ const jsonParser = require('body-parser').json();
 const {Router} = require('express');
 const s3Upload = require('../lib/s3-upload-middleware.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
-const User = require('../model/user.js');
+const Profile = require('../model/profile.js');
 
 const profileRouter = module.exports = new Router();
 
-profileRouter.post('/api/profile/:id', jsonParser, basicAuth, (req, res, next) => {
+profileRouter.post('/api/profile/:id', jsonParser, bearerAuth, s3Upload('image'), (req, res, next) => {
+  new Profile({
+    name: req.body.name,
+    residenceId: req.body.residenceId.toString(),
+    phone: req.body.phone,
+    bio: req.body.bio,
+    photoURI: req.s3Data.Location,
+    userId: req.params.id,
+  })
+    .save()
+    .then(profile => res.json(profile))
+    .catch(next);
+});
 
+profileRouter.put('/api/profile/:id', bearerAuth, (req, res, next) => {
+  Profile.findOneAndUpdate({ userId: req.params.id }, req.body, { new: true })
+    .then(profile => res.json(profile))
+    .catch(next);
 });
 
 profileRouter.get('/api/profile/:id', (req, res, next) => {
-
-});
-
-petRouter.post('/api/pets', bearerAuth, s3Upload('image'), (req, res, next) => {
-  new Pet({
-    name: req.body.name,
-    type: req.body.type,
-    photoURI: req.s3Data.Location,
-    userID: req.user._id.toString(),
-  })
-  .save()
-  .then(pet => res.json(pet))
-  .catch(next);
+  Profile.findOne({ userId: req.params.id })
+    .then(profile => res.json(profile))
+    .catch(next);
 });
