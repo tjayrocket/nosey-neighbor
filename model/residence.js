423aca2a701942ad90c12c52aa36dd5c7614b2dc
StephanieDover <doverstephaniem@gmail.com>
2017-07-11 11:39:52 -0700

    Update residence.js

--- model/residence.js
+++ model/residence.js
@@ -3,15 +3,9 @@
 const mongoose = require('mongoose');
 
 const residenceSchema = mongoose.Schema({
-<<<<<<< HEAD
-  address: {type: String, required: true, minlength: 1, unique: true},
-  occupants: [{type: String}],
-  incidents: [{type: mongoose.Schema.Types.ObjectId, ref: 'incident'}],
-=======
   address: { type: String, required: true, minlength: 1, immutable: true },
   occupants: [{ type: String }],
   incidents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'incident' }]
->>>>>>> d3d0f88538b2ecd25f086e0610acdf9ba8185ec0
 });
 
 module.exports = mongoose.model('residence', residenceSchema);
