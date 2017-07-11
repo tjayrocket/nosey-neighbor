5146eed9cdeda4bd2d4d4eb3a31d268cd75ac862
Spencer Gietzen <Speng1516@comcast.net>
2017-07-11 10:06:42 -0700

    rewrote and added tests to profile test

--- test/profile-test.js
+++ test/profile-test.js
@@ -7,6 +7,7 @@ const expect = require('expect');
 const server = require('../lib/server.js');
 const cleanDB = require('./lib/clean-db.js');
 const mockUser = require('./lib/mock-user.js');
+const mockResidence = require('./lib/mock-residence.js');
 
 const API_URL = process.env.API_URL;
 
@@ -19,29 +20,27 @@ describe('Testing Profile Model', () => {
     it('should return 200', () => {
       return mockUser.createOne()
         .then(userData => {
-          return superagent.post(`${API_URL}/api/profile`)
+          return superagent.post(`${API_URL}/api/profile/${userData._id}`)
             .send({
-              id: userData._id,
-              name: 'Yooooo',
+              name: 'Phil',
               phone: 1236530000,
-              bio: 'yo whattup',
+              bio: 'I am Phil',
             })
             .then(res => {
               expect(res.status).toEqual(200);
-              expect(res.id).toEqual(userData._id);
-              expect(res.name).toEqual('Yooooo');
+              expect(res._id).toEqual(userData._id);
+              expect(res.name).toEqual('Phil');
               expect(res.phone).toEqual(1236530000);
-              expect(res.bio).toEqual('yo whattup');
+              expect(res.bio).toEqual('I am Phil');
             });
         });
     });
     it('should return 400 bad request', () => {
       return mockUser.createOne()
         .then(userData => {
-          return superagent.post(`${API_URL}/api/profile`)
+          return superagent.post(`${API_URL}/api/profile/${userData._id}`)
             .send({
-              id: userData._id,
-              yo: 'yo',
+              nope: 'non existent',
             })
             .then(res => {
               throw res;
@@ -54,14 +53,32 @@ describe('Testing Profile Model', () => {
   });
 
   describe('Testing GET - Incident Array', () => {
-    it('should return 200 - blah blah', () => {
-
+    it('should return 200', () => {
+      return mockResidence.createOne()
+        .then(residence => {
+          return mockUser.createOne()
+            .then(userData => {
+              return superagent.post(`${API_URL}/api/profile/${userData._id}`)
+                .send({
+                  residenceId: residence._id,
+                  name: 'Phil',
+                  phone: 1236530000,
+                  bio: 'I am Phil',
+                })
+                .then(() => {
+                  return superagent.get(`${API_URL}/api/profile/${userData._id}`)
+                    .then(res => {
+                      expect(res.status).toEqual(200);
+                      expect(res.userId).toEqual(userData._id);
+                      expect(res.name).toEqual('Phil');
+                      expect(res.phone).toEqual(1236530000);
+                      expect(res.bio).toEqual('I am Phil');
+                      expect(res.residenceId).toEqual(residence._id);
+                    });
+                });
+            });
+        });
     });
-
-    it('should return 200 - blah blah', () => {
-
-    });
-
     it('Should return with 404 not found', () => {
       return superagent.get(`${API_URL}/api/profile/asdasdasdasd`)
         .then(res => {
