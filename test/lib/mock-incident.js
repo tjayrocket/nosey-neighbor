bedb85f23c630a9b353e57f0d9a51695aa807084
0smium <steve.walsh@transfueler.com>
2017-07-11 21:13:53 -0700

    got first incident test running, but there is a problem in the incident model that is commented out

--- test/lib/mock-incident.js
+++ test/lib/mock-incident.js
@@ -3,27 +3,34 @@
 const faker = require('faker');
 const Incident = require('../../model/incident.js');
 const mockUser = require('./mock-user.js');
+const mockResidence = require('./mock-residence.js')
 
 const mockIncident = module.exports = {};
 
-let mockUserData = mockUser.createOne();
-
 mockIncident.createOne = () => {
-  let result = {};
-  result.userId = mockUserData.user._id,
-  result.type = faker.lorem.word();
-  result.description = faker.lorem.sentence();
-  result.comments = faker.lorem.sentence();
-
-  return new Incident({
-    userId: result.userId,
-    type: result.type,
-    description: result.description,
-    comments: result.comments,
-  })
-    .save()
-    .then(incident => {
-      result.id = incident._id;
-      return result;
+  return mockUser.createOne()
+    .then(mockUserData => {
+      // console.log('mockUserData: ', mockUserData);
+      let result = {};
+      result.userId = mockUserData.user._id;
+      result.type = faker.lorem.word();
+      result.description = faker.lorem.sentence();
+      result.userToken = mockUserData.token;
+      return mockResidence.createOne()
+        .then(mockResidenceData => {
+          result.residenceId = mockResidenceData.id;
+          return new Incident({
+            userId: result.userId,
+            type: result.type,
+            description: result.description,
+            residenceId: result.residenceId,
+          })
+            .save()
+            .then(incident => {
+              result.id = incident._id;
+              // console.log('result: ', result);
+              return result;
+            });
+        });
     });
 };
